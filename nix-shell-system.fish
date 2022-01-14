ca175da6782860d3654626214e00ab8e7f4aea56
Anselm Schüler <mail@anselmschueler.com>
2022-01-14 21:57:01 +0100

    change arguments to nix-shell-system

--- nix-shell-system.fish
+++ nix-shell-system.fish
@@ -1,3 +1,3 @@
 function nix-shell-system
-    nix shell --impure --expr "with builtins; (getFlake \"/etc/nixos\").inputs.nixpkgs.legacyPackages.\${currentSystem}.$argv[1..-2]" -c $argv[-1]
+    nix shell --impure --expr "with builtins; (getFlake \"/etc/nixos\").inputs.nixpkgs.legacyPackages.\${currentSystem}.$argv[1]" -c $argv[2..-1]
 end
