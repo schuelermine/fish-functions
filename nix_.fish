18459925172f83f6605fa0e9820c6dee24545d3b
Anselm Schüler <mail@anselmschueler.com>
2022-01-26 19:53:50 +0100

    Fix nix_

--- nix_.fish
+++ nix_.fish
@@ -1,9 +1,9 @@
 set expr 'let pkgs = (builtins.getFlake "/etc/nixos").inputs.nixpkgs.legacyPackages.${builtins.currentSystem};
-in pkgs.'$argv[1]
+in pkgs.'$argv[2]
 switch $argv[1]
   case "run"
-    nix run --impure --expr $expr -- $argv[2..-1]
+    nix run --impure --expr $expr -- $argv[3..-1]
   case "shell"
-    set -q argv[2] || set argv[2] $SHELL
-    nix shell --impure --expr $expr -c $argv[2..-1]
+    set -q argv[3] || set argv[3] $SHELL
+    nix shell --impure --expr $expr -c $argv[3..-1]
 end
