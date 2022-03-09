f83dc9c72dab701503a148f7391cdb0eca56add7
Anselm Schüler <mail@anselmschueler.com>
2022-03-09 16:11:55 +0100

    Update nix_

--- nix_.fish
+++ nix_.fish
@@ -3,7 +3,7 @@ switch $argv[1]
   case "repl"
     nix repl ~/repl.nix
   case "run"
-    nix run --impure --expr $expr -- $argv[3..-1]
+    nix run --impure --expr $expr "" -- $argv[3..-1]
   case "shell"
     set -q argv[3] || set argv[3] $SHELL
     nix shell --impure --expr $expr -c $argv[3..-1]
